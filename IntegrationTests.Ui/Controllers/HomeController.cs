9481c2bce7e68ed3fb5ffdea53704116b83a6622
sgmeyer <sgmeyer@gmail.com>
2014-12-17 10:36:36 -0600

    initial commit

---
+++ IntegrationTests.Ui/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Driver;
+using System.Web.Mvc;
+using MongoDB.Driver.Builders;
+
+namespace IntegrationTests.Ui.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult LatestTestResult()
+        {
+            var mongoconnection = ConfigurationManager.AppSettings["mongo"];
+            var database = ConfigurationManager.AppSettings["mongodb"];
+            var mongoClient = new MongoClient(mongoconnection);
+            var server = mongoClient.GetServer();
+
+            server.Connect();
+
+            var db = server.GetDatabase(database);
+            var collection = db.GetCollection("testresults");
+
+            var query = new SortByBuilder();
+            query.Descending("_id");
+
+            var latestDocument = collection.FindAllAs<BsonDocument>().SetSortOrder(query).SetLimit(1);
+            server.Disconnect();
+
+            var jsonWriterSettings = new JsonWriterSettings { OutputMode = JsonOutputMode.Strict };
+            var json = latestDocument.ToJson(jsonWriterSettings);
+
+            return Content(json, "application/json");
+        }
+    }
+}
---
+++ IntegrationTests.Ui/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IntegrationTests.Ui.Startup))]
+namespace IntegrationTests.Ui
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
